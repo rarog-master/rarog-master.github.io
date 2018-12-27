8dc7b60c7791783407bfa82a64a00c072bfdafe3
rarog-master.github.io <verimp@rambler.ru>
2018-12-27 08:58:31 -0700

    2

---
+++ php/komanda.php
@@ -0,0 +1,60 @@
+<!-- team -->
+<section class="team py-5 my-md-5 my-3">
+	<div class="container">
+		<div class="heading">
+			<h3 class="heading text-uppercase mb-5 pb-3"><span>Quality</span> Team </h3>
+			<h4 class="position cont">TEAM</h4>
+		</div>
+		<div class="row text-center">
+			<div class=" col-md-3 col-sm-6 col-6 mb-md-0 mb-5 profile">
+				<div class="img-box">
+					<img src="images/t1.jpg" alt="">
+					<ul class="text-center">
+					<li><a href="#"><span class="fab fa-facebook-f"></span></a></li>
+						<li><a href="#"><span class="fab fa-twitter"></span></a></li>
+						<li><a href="#"><span class="fa fa-rss"></span></a></li>
+					</ul>
+				</div>
+					<h4 class="mt-3">Eliza</h4>
+					<p class="mt-2">General manager</p>
+			</div>
+			<div class="col-md-3 col-sm-6 col-6 mb-md-0 mb-5  profile">
+				<div class="img-box">
+					<img src="images/t2.jpg" alt="">
+					<ul class="text-center">
+					   <li><a href="#"><span class="fab fa-facebook-f"></span></a></li>
+					   <li><a href="#"><span class="fab fa-twitter"></span></a></li>
+					   <li><a href="#"><span class="fa fa-rss"></span></a></li>
+					</ul>
+				</div>
+					<h4 class="mt-3">Charles</h4>
+					<p class="mt-2">General manager</p>
+			</div>
+			<div class="col-md-3 col-sm-6 col-6 profile">
+				<div class="img-box">
+					<img src="images/t3.jpg" alt="">
+					<ul class="text-center">
+					   <li><a href="#"><span class="fab fa-facebook-f"></span></a></li>
+					   <li><a href="#"><span class="fab fa-twitter"></span></a></li>
+					   <li><a href="#"><span class="fa fa-rss"></span></a></li>
+					</ul>
+				</div>
+					<h4 class="mt-3">Johnson</h4>
+					<p class="mt-2">General manager</p>
+			</div>
+			<div class="col-md-3 col-sm-6 col-6 profile">
+				<div class="img-box">
+					<img src="images/t4.jpg" alt="">
+					<ul class="text-center">
+					   <li><a href="#"><span class="fab fa-facebook-f"></span></a></li>
+					   <li><a href="#"><span class="fab fa-twitter"></span></a></li>
+					   <li><a href="#"><span class="fa fa-rss"></span></a></li>
+					</ul>
+				</div>
+					<h4 class="mt-3">Michael</h4>
+					<p class="mt-2">General manager</p>
+			</div>
+		</div>
+	</div>
+</section>
+<!--//team -->
---
+++ php/mini-diafilm.php
@@ -0,0 +1,86 @@
+<div class="inner_sec_info_agile_w3_info">
+			<div class="help_full">
+				<ul class="rslides" id="slider4">
+					<li>
+						<div class="respon_info_img">
+							<img src="images/master32.jpg" class="img-responsive" alt="Estate Land">
+						</div>
+						<div class="banner_bottom_left">
+							<h4 class="my-3 text-capitalize"> Property value - 215$</h4>
+							<ul class="property-info mb-3">
+								<li><span class="fas fa-building"></span> 4600sqft</li>
+								<li><span class="fas fa-bed"></span> 2 Bedrooms</li>
+								<li><span class="fas fa-bath"></span> 2 Bathrooms</li>
+								<li><span class="fas fa-car"></span> 1 Car parking</li>
+							</ul>
+							<p>Maecenas quis neque libero. Class aptent taciti. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+								convallis diam consequat magna vulputate malesuada. Cras a ornare elit. Nulla viverra pharetra sem, eget pulvinar
+								neque pharetra ac.</p>
+							<div class="ab_button">
+								<a class="btn btn-primary btn-lg hvr-underline-from-left" href="single.html" role="button">Read More </a>
+							</div>
+						</div>
+					</li>
+					<li>
+						<div class="respon_info_img">
+							<img src="images/6.jpg" class="img-responsive" alt="Estate Land">
+						</div>
+						<div class="banner_bottom_left">
+							<h4 class="my-3 text-capitalize"> Property value - 215$</h4>
+							<ul class="property-info mb-3">
+								<li><span class="fas fa-building"></span> 4600sqft</li>
+								<li><span class="fas fa-bed"></span> 2 Bedrooms</li>
+								<li><span class="fas fa-bath"></span> 2 Bathrooms</li>
+								<li><span class="fas fa-car"></span> 1 Car parking</li>
+							</ul>
+							<p>Maecenas quis neque libero. Class aptent taciti. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+								convallis diam consequat magna vulputate malesuada. Cras a ornare elit. Nulla viverra pharetra sem, eget pulvinar
+								neque pharetra ac.</p>
+							<div class="ab_button">
+								<a class="btn btn-primary btn-lg hvr-underline-from-left" href="single.html" role="button">Read More </a>
+							</div>
+						</div>
+					</li>
+					<li>
+						<div class="respon_info_img">
+							<img src="images/7.jpg" class="img-responsive" alt="Estate Land">
+						</div>
+						<div class="banner_bottom_left">
+							<h4 class="my-3 text-capitalize"> Property value - 215$</h4>
+							<ul class="property-info mb-3">
+								<li><span class="fas fa-building"></span> 4600sqft</li>
+								<li><span class="fas fa-bed"></span> 2 Bedrooms</li>
+								<li><span class="fas fa-bath"></span> 2 Bathrooms</li>
+								<li><span class="fas fa-car"></span> 1 Car parking</li>
+							</ul>
+							<p>Maecenas quis neque libero. Class aptent taciti. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+								convallis diam consequat magna vulputate malesuada. Cras a ornare elit. Nulla viverra pharetra sem, eget pulvinar
+								neque pharetra ac.</p>
+							<div class="ab_button">
+								<a class="btn btn-primary btn-lg hvr-underline-from-left" href="single.html" role="button">Read More </a>
+							</div>
+						</div>
+					</li>
+					<li>
+						<div class="respon_info_img">
+							<img src="images/5.jpg" class="img-responsive" alt="Estate Land">
+						</div>
+						<div class="banner_bottom_left">
+							<h4 class="my-3 text-capitalize"> Property value - 215$</h4>
+							<ul class="property-info mb-3">
+								<li><span class="fas fa-building"></span> 4600sqft</li>
+								<li><span class="fas fa-bed"></span> 2 Bedrooms</li>
+								<li><span class="fas fa-bath"></span> 2 Bathrooms</li>
+								<li><span class="fas fa-car"></span> 1 Car parking</li>
+							</ul>
+							<p>Maecenas quis neque libero. Class aptent taciti.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+								convallis diam consequat magna vulputate malesuada. Cras a ornare elit. Nulla viverra pharetra sem, eget pulvinar
+								neque pharetra ac.</p>
+							<div class="ab_button">
+								<a class="btn btn-primary btn-lg hvr-underline-from-left" href="single.html" role="button">Read More </a>
+							</div>
+						</div>
+					</li>
+				</ul>
+			</div>
+		</div>
