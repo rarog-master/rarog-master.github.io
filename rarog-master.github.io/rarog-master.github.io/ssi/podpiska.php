9452bf1d849a0484057485ac57ae8a7261394db5
rarog-master.github.io <verimp@rambler.ru>
2018-12-28 09:42:37 -0700

    5

---
+++ rarog-master.github.io/rarog-master.github.io/ssi/podpiska.php
@@ -0,0 +1,15 @@
+<section class="newsletter py-5">
+	<div class="container">
+		<div class="newslettergrids row">
+			<div class="col-sm-6 newsleft">
+				<h3>Получать уведомления на почту !</h3>
+			</div>
+			<div class="col-sm-6 newsright">
+				<form action="#" method="post">
+					<input type="email" placeholder="Ваш эл.ящик..." name="email" required="">
+					<input type="submit" value="Да">
+				</form>
+			</div>
+		</div>
+	</div>
+</section>
---
+++ rarog-master.github.io/rarog-master.github.io/ssi/zakazchiki.php
@@ -0,0 +1,91 @@
+<!-- testimonals section -->
+<div class="testimonials py-5">
+		<div class="container">
+		<div class="heading">
+			<h3 class="heading text-uppercase mb-5 pb-3"><span>Clients</span> Reviews </h3>
+			<h4 class="position cont">REVIEWS</h4>
+		</div>
+			<div class="agileits-feedback-grids">
+				<div id="owl-demo" class="owl-carousel owl-theme">
+					<div class="item">
+						<div class="test-review">
+								<i class="fa fa-quote-left" aria-hidden="true"></i>
+								<p>Polite sadipscing elitr, sed diam is nonumy is eirmod tempor invidunt ut labore et dolore magna aliquyam  </p>
+							<div class="img-agile my-3">
+								<img src="images/t1.jpg" class="img-responsive" alt=""/>
+							</div>
+							<div class="info">
+								<h5>Rita</h5>
+								<p>Sadipscing</p>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="test-review">
+							<i class="fa fa-quote-left" aria-hidden="true"></i>
+							<p>Polite sadipscing elitr, sed diam is nonumy is eirmod tempor invidunt ut labore et dolore magna aliquyam  </p>		<div class="img-agile my-3">
+								<img src="images/t2.jpg" class="img-responsive" alt=""/>
+							</div>
+							<div class="info">
+								<h5>Williams</h5>
+								<p>Sadipscing</p>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="test-review">
+								<i class="fa fa-quote-left" aria-hidden="true"></i>
+								<p>Polite sadipscing elitr, sed diam is nonumy is eirmod tempor invidunt ut labore et dolore magna aliquyam   </p>
+							<div class="img-agile my-3">
+								<img src="images/t3.jpg" class="img-responsive" alt=""/>
+							</div>
+							<div class="info">
+								<h5>Laila</h5>
+								<p>Sadipscing</p>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="test-review">
+								<i class="fa fa-quote-left" aria-hidden="true"></i>
+								<p>Polite sadipscing elitr, sed diam is nonumy is eirmod tempor invidunt ut labore et dolore magna aliquyam  </p>
+							<div class="img-agile my-3">
+								<img src="images/t4.jpg" class="img-responsive" alt=""/>
+							</div>
+							<div class="info">
+								<h5>Stella</h5>
+								<p>Sadipscing</p>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="test-review">
+								<i class="fa fa-quote-left" aria-hidden="true"></i>
+								<p>Polite sadipscing elitr, sed diam is nonumy is eirmod tempor invidunt ut labore et dolore magna aliquyam  </p>
+							<div class="img-agile my-3">
+								<img src="images/t1.jpg" class="img-responsive" alt=""/>
+							</div>
+							<div class="info">
+								<h5>Rick</h5>
+								<p>Sadipscing</p>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="test-review">
+								<i class="fa fa-quote-left" aria-hidden="true"></i>
+								<p>Polite sadipscing elitr, sed diam is nonumy is eirmod tempor invidunt ut labore et dolore magna aliquyam  </p>
+							<div class="img-agile my-3">
+								<img src="images/t2.jpg" class="img-responsive" alt=""/>
+							</div>
+							<div class="info">
+								<h5>Richard</h5>
+								<p>Sadipscing</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+<!-- /testimonals section -->
